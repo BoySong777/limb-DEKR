6cb2ab12ac1db2b7fa28cbfb5c477e34db3a8dbf
BoySong777 <1572798848@qq.com>
2023-03-06 09:14:32 +0800

    实验四（-1-2-3-4）： 1.添加oks loss 2，改进 oks loss 3. 仿照focal loss给smooth l1 loss添加权重 4， 改进3

--- lib/core/loss.py
+++ lib/core/loss.py
@@ -14,6 +14,7 @@ import logging
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -49,17 +50,88 @@ class OffsetsLoss(nn.Module):
         return loss
 
 
+class OKSLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def oks_loss(self, pred, gt, weights, sigmas):
+        '''
+
+        Args:
+            pred: (N, 2K, W, H)
+            gt: (N, 2K, W, H)
+            weights: groundtruth中，每个关键点对应人体的面积的倒数：1/s.  (N, 2K, W, H)
+            sigmas: 各个关键点的权重:(K,)
+
+        Returns:
+
+        '''
+        sigmas = torch.tensor(sigmas, device=gt.device)
+        # 计算关键点之间的欧几里得距离
+        dx = (pred[:, 0::2] - gt[:, 0::2])**2
+        dy = (pred[:, 1::2] - gt[:, 1::2])**2
+        dist = dx + dy
+        # 求得关键点归一化因子的平方，这里为什么乘以2，可能是因为坐标有两个值（x和y）
+        vars = (sigmas * 2) ** 2
+        vars = vars[None, :, None, None]
+        # 这里乘以weights[:, 0::2] > 0的目的是为了让groundtrue中未标注的点不参与loss，也就是为了实现oks公式中的δ（v_n > 0）
+        oks = torch.mul(torch.exp(-dist/(1.0/weights[:, 0::2]+np.spacing(1))/vars/2.0), weights[:, 0::2] > 0).sum(axis=1)
+        # 除以关键点的数量，平均一下
+        w = (weights[:, 0::2] > 0).sum(axis=1)
+        # 这个num_pos是人数，也就是gt中一共被标记了多少个人，实际图片中的人在gt中被标记了64次，
+        num_pos = torch.nonzero(w).size()[0]
+        oks = oks / (w + np.spacing(1))
+        loss = (w > 0).type(torch.float) - oks
+        return loss, num_pos
+
+    def forward(self, pred, gt, weights, sigmas):
+        assert pred.size() == gt.size()
+        # num_pos = torch.nonzero(weights > 0).size()[0]
+        loss, num_pos = self.oks_loss(pred, gt, weights, sigmas)
+        if num_pos == 0:
+            num_pos = 1.
+        loss = loss.sum() / num_pos
+        return loss
+
+
+class OffsetsLossAddWeight(nn.Module):
+    '''
+        仿照focal loss 给偏移量损失增加权重，具体来说，困难关键点的偏移量的权重应该尽可能地大。
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def smooth_l1_loss(self, pred, gt, weight, beta=1. / 9):
+        l1_loss = torch.abs(pred - gt)
+        cond = l1_loss < beta
+        # loss = (1 - torch.exp(- l1_loss * weight)) * torch.where(cond, 0.5*l1_loss**2/beta, l1_loss-0.5*beta)
+        loss = l1_loss * torch.where(cond, 0.5*l1_loss**2/beta, l1_loss-0.5*beta)
+        return loss
+
+    def forward(self, pred, gt, weights):
+        assert pred.size() == gt.size()
+        num_pos = torch.nonzero(weights > 0).size()[0]
+        loss = self.smooth_l1_loss(pred, gt, weights) * weights
+        if num_pos == 0:
+            num_pos = 1.
+        loss = loss.sum() / num_pos
+        return loss
+
+
 class MultiLossFactory(nn.Module):
     def __init__(self, cfg):
         super().__init__()
 
         self.num_joints = cfg.MODEL.NUM_JOINTS
         self.bg_weight = cfg.DATASET.BG_WEIGHT
+        self.sigmas = cfg.DATASET.KEYPOINTS_SIGMAS
 
         self.heatmap_loss = HeatmapLoss() if cfg.LOSS.WITH_HEATMAPS_LOSS else None
         self.heatmap_loss_factor = cfg.LOSS.HEATMAPS_LOSS_FACTOR
 
-        self.offset_loss = OffsetsLoss() if cfg.LOSS.WITH_OFFSETS_LOSS else None
+        # self.offset_loss = OffsetsLoss() if cfg.LOSS.WITH_OFFSETS_LOSS else None
+        # self.offset_loss = OKSLoss() if cfg.LOSS.WITH_OFFSETS_LOSS else None
+        self.offset_loss = OffsetsLossAddWeight() if cfg.LOSS.WITH_OFFSETS_LOSS else None
         self.offset_loss_factor = cfg.LOSS.OFFSETS_LOSS_FACTOR
         # 增加肢体偏移量的损失。
         self.limbs_offset_loss = OffsetsLoss() if cfg.LOSS.WITH_OFFSETS_LOSS else None
@@ -72,6 +144,7 @@ class MultiLossFactory(nn.Module):
             heatmap_loss = None
         
         if self.offset_loss:
+           # offset_loss = self.offset_loss(poffset, offset, offset_w)
             offset_loss = self.offset_loss(poffset, offset, offset_w)
             offset_loss = offset_loss * self.offset_loss_factor
             # 和关键点偏移量类似，获取肢体中心点的偏移量
